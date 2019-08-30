33cb085bacab95ed336fbe7096e33033fdbb73ea
savvysol <mike@savvysolutions.com>
2019-08-29 19:36:04 -0700

    made a change on nice branch.

--- first_file.py
+++ first_file.py
@@ -1,4 +1,7 @@
 print (f'this is a master file')
-print('this is a branch line for nice_branch')
+print('this is the next line.')
 print("This is a master copy")
 print("This is a second master line")
+print("This is also in the master")
+
+print('Here is a line from nice_branch.')
